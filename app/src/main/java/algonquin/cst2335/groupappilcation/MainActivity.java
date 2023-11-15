ae372879ff257c8ed62785d33c10abc3a5fd4821
Christopher St.Aubin <stau0055@algonquinlive.com>
2023-11-15 11:43:27 -0500

    Added Toast and Snackbar

--- app/src/main/java/algonquin/cst2335/groupappilcation/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/groupappilcation/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import algonquin.cst2335.groupappilcation.databinding.ActivityMainBinding;
 
@@ -21,21 +24,33 @@ public class MainActivity extends AppCompatActivity {
         binding.dictionaryButton.setOnClickListener( click -> {
 
             startActivity(new Intent(this, DictionaryMain.class));
+            Toast.makeText(this, "Click this button", Toast.LENGTH_LONG).show();
+
+            Snackbar.make(binding.dictionaryButton, "Click this button", Snackbar.LENGTH_LONG).show();
         });
 
         binding.recipeButton.setOnClickListener( click -> {
 
             startActivity(new Intent(this, RecipeSearchMain.class));
+            Toast.makeText(this, "Click this button", Toast.LENGTH_LONG).show();
+
+            Snackbar.make(binding.recipeButton, "Click this button", Snackbar.LENGTH_LONG).show();
         });
 
         binding.sunriseButton.setOnClickListener( click -> {
 
             startActivity(new Intent(this, SunsetSunriseMain.class));
+            Toast.makeText(this, "Click this button", Toast.LENGTH_LONG).show();
+
+            Snackbar.make(binding.sunriseButton, "Click this button", Snackbar.LENGTH_LONG).show();
         });
 
         binding.songButton.setOnClickListener( click -> {
 
             startActivity(new Intent(this, SongSearchMain.class));
+            Toast.makeText(this, "Click this button", Toast.LENGTH_LONG).show();
+
+            Snackbar.make(binding.songButton, "Click this button", Snackbar.LENGTH_LONG).show();
         });
 
     }
--- app/src/main/java/algonquin/cst2335/groupappilcation/pages/dictionaryData/dictionaryAPIdata.java
+++
@@ -1,2 +0,0 @@
-package algonquin.cst2335.groupappilcation.pages.dictionaryData;public class dictionaryAPIdata {
-}
