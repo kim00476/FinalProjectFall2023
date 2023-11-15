49ca711dda78dd944edb13568c5b078d4123c6b8
Christopher St.Aubin <stau0055@algonquinlive.com>
2023-11-15 17:54:14 -0500

    Adding tool bar and edittext to dictionary page

--- app/src/main/java/algonquin/cst2335/groupappilcation/DictionaryMain.java
+++ app/src/main/java/algonquin/cst2335/groupappilcation/DictionaryMain.java
@@ -3,12 +3,32 @@ package algonquin.cst2335.groupappilcation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import algonquin.cst2335.groupappilcation.databinding.ActivityDictionaryMainBinding;
+
 
 public class DictionaryMain extends AppCompatActivity {
+
+ActivityDictionaryMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dictionary_main);
+
+        binding = ActivityDictionaryMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.dictionaryToolbar);
+
+        }
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            super.onCreateOptionsMenu(menu);
+
+            getMenuInflater().inflate(R.menu.dictionary_menu, menu);
+
+            return true;
     }
 }
