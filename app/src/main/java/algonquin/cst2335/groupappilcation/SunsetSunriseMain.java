e61844d8bfab11dff2ef3ddf819e484fe6c1ab32
Christopher St.Aubin <stau0055@algonquinlive.com>
2023-11-18 17:23:31 -0500

    Added SharedPreferences for Dictionary page and recycler view

--- app/src/main/java/algonquin/cst2335/groupappilcation/SunsetSunriseMain.java
+++ app/src/main/java/algonquin/cst2335/groupappilcation/SunsetSunriseMain.java
@@ -10,5 +10,6 @@ public class SunsetSunriseMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sunset_sunrise_main);
+
     }
 }
