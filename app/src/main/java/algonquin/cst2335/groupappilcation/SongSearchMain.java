ebee59b994814ca2ec013f48223284e0bc0140fa
Miok Kim <kim00476@algonquinlive.com>
2023-11-19 15:28:43 -0500

    start toolbar

--- app/src/main/java/algonquin/cst2335/groupappilcation/SongSearchMain.java
+++ app/src/main/java/algonquin/cst2335/groupappilcation/SongSearchMain.java
@@ -3,12 +3,31 @@ package algonquin.cst2335.groupappilcation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import algonquin.cst2335.groupappilcation.databinding.ActivityDictionaryMainBinding;
+import algonquin.cst2335.groupappilcation.databinding.ActivitySongSearchMainBinding;
 
 public class SongSearchMain extends AppCompatActivity {
+
+    ActivitySongSearchMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_song_search_main);
+
+        binding = ActivitySongSearchMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.SongToolbar);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.song_menu, menu);
+
+        return true;
+    }
+}//last
