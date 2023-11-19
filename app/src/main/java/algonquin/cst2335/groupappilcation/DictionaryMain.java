388639f92bfefb810936255ad31a825a6812cec8
Christopher St.Aubin <stau0055@algonquinlive.com>
2023-11-19 14:57:00 -0500

    Trying to add API and database

--- app/src/main/java/algonquin/cst2335/groupappilcation/DictionaryMain.java
+++ app/src/main/java/algonquin/cst2335/groupappilcation/DictionaryMain.java
@@ -1,6 +1,8 @@
 package algonquin.cst2335.groupappilcation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -8,20 +10,51 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import algonquin.cst2335.groupappilcation.Data.DictionaryModel;
 import algonquin.cst2335.groupappilcation.databinding.ActivityDictionaryMainBinding;
 
 
 public class DictionaryMain extends AppCompatActivity {
 
 ActivityDictionaryMainBinding binding;
-SharedPreferences prefs;
+RequestQueue queue = null;
+private RecyclerView.Adapter myAdapter;
+ArrayList<DictionaryItem> rDef;
+DictionaryModel dictionaryModel;
+DictionaryItemDAO wDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        DictionaryDatabase db = Room.databaseBuilder(getApplicationContext(), DictionaryDatabase.class, "dictionary-db")
+                .build();
+        wDAO = db.dictionaryItemDAO();
+
+        rDef = dictionaryModel.rDef.getValue();
+        if (rDef == null)
+            dictionaryModel.rDef.postValue(rDef = new ArrayList<DictionaryItem>());
+
+
+
+        queue = Volley.newRequestQueue(this);
+
         binding = ActivityDictionaryMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         setSupportActionBar(binding.dictionaryToolbar);
 
@@ -35,9 +68,49 @@ SharedPreferences prefs;
             String wordSearched = word.getText().toString();
             editor.putString("WordSearched", wordSearched);
             editor.apply();
-        });
 
-        }
+            String stringUrl = null;
+            try {
+                stringUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+                        + URLEncoder.encode(wordSearched, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringUrl, null,
+                    (response) -> {
+                        try {
+
+                            JSONArray meaningsArray = response.getJSONArray("meanings");
+
+                            for (int i = 0; i < meaningsArray.length(); i++) {
+                                JSONObject meaningsObject = meaningsArray.getJSONObject(i);
+
+                                JSONArray definitionsArray = meaningsObject.getJSONArray("definitions");
+
+                                for (int j = 0; j < definitionsArray.length(); j++) {
+                                    JSONObject definitionObject = definitionsArray.getJSONObject(j);
+
+                                    // Get the definition
+                                    String definition = definitionObject.getString("definition");
+
+                                    // Add the word and definition to the adapter
+                                    myAdapter.addData(new DictionaryItem(wordSearched, definition));
+                                }
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    },
+                    (error) -> {
+                        error.printStackTrace();
+                    });
+
+            // Add the request to the RequestQueue
+            queue.add(request);
+        });
+    }
 
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
