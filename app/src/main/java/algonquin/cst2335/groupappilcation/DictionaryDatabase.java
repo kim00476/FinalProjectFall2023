b90753b7558c097b7c8adc279567e9b0d03e89a5
Christopher St.Aubin <stau0055@algonquinlive.com>
2023-12-01 14:39:39 -0500

    Added JavaDocs for Dictionary classes and Main activity

--- app/src/main/java/algonquin/cst2335/groupappilcation/DictionaryDatabase.java
+++ app/src/main/java/algonquin/cst2335/groupappilcation/DictionaryDatabase.java
@@ -3,7 +3,16 @@ package algonquin.cst2335.groupappilcation;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+/**
+ * The Database class for managing the persistence of DictionaryItem entities using RoomDatabase.
+ */
 @Database(entities = {DictionaryItem.class}, version=1)
 public abstract class DictionaryDatabase extends RoomDatabase {
+
+    /**
+     * Returns an instance of the associated DAO (Data Access Object) for DictionaryItem.
+     *
+     * @return The DictionaryItemDAO for performing database operations on DictionaryItem entities.
+     */
     public abstract DictionaryItemDAO dictionaryItemDAO();
 }
